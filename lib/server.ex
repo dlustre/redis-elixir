ea01900c3ab2804f2638fc7b5863c0c58d00943d
dlustre <dennis.lustre@gmail.com>
2024-11-11 22:55:59 -0800

    codecrafters submit [skip ci]

--- lib/server.ex
+++ lib/server.ex
@@ -9,6 +9,24 @@ defmodule Server do
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
+  def handle("*1\r\n$4\r\nPING\r\n", client), do: :gen_tcp.send(client, "+PONG\r\n")
+
+  def handle(_, _), do: raise("Huh")
+
+  def serve(client) do
+    case :gen_tcp.recv(client, 0) do
+      {:ok, data} -> handle(data, client)
+      _ -> raise "Todo"
+    end
+
+    serve(client)
+  end
+
+  def loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+  end
+
   @doc """
   Listen for incoming connections
   """
@@ -16,8 +34,7 @@ defmodule Server do
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, client} = :gen_tcp.accept(socket)
 
-    :gen_tcp.send(client, "+PONG\r\n")
+    loop_acceptor(socket)
   end
 end
