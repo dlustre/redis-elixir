57a99614bb96e9bc628c8670afb9d19e24e3c5b8
dlustre <dennis.lustre@gmail.com>
2024-11-15 03:35:28 -0800

    codecrafters submit [skip ci]

--- lib/server.ex
+++ lib/server.ex
@@ -1,3 +1,169 @@
+defmodule Util do
+  require(ExUnit.Assertions)
+
+  def assert_equals(actual, expected), do: ExUnit.Assertions.assert(actual == expected)
+end
+
+defmodule Rdb do
+  @header "REDIS0011"
+  @eof 0xFF
+  @selectdb 0xFE
+  @string 0x00
+  @expiretime 0xFD
+  @expiretime_ms 0xFC
+  @resizedb 0xFB
+  @aux 0xFA
+
+  @next_six <<0b00::2>>
+  @next_fourteen <<0b01::2>>
+  @next_thirty_two <<0b10::2>>
+  @special <<0b11::2>>
+
+  defmodule Section do
+    @enforce_keys [:kind, :data]
+    defstruct [:kind, :data]
+  end
+
+  def test_length_encoding() do
+    consume(<<0x0A>>, :length_encoded) |> elem(0) |> Util.assert_equals(10)
+
+    consume(<<0x42, 0xBC>>, :length_encoded)
+    |> elem(0)
+    |> Util.assert_equals(700)
+
+    consume(<<0x80, 0x00, 0x00, 0x42, 0x68>>, :length_encoded)
+    |> elem(0)
+    |> Util.assert_equals(17000)
+
+    consume(<<0b11::2>>, :length_encoded)
+  end
+
+  def test_str_encoding() do
+    consume(
+      <<0x0D, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21>>,
+      :str_encoded
+    )
+    |> elem(0)
+    |> Util.assert_equals("Hello, World!")
+
+    consume(
+      <<0xC0, 0x7B, 0xFF>>,
+      :str_encoded
+    )
+    |> elem(0)
+    |> Util.assert_equals(123)
+
+    consume(
+      <<0xC1, 0x39, 0x30>>,
+      :str_encoded
+    )
+    |> elem(0)
+    |> Util.assert_equals(12345)
+
+    consume(
+      <<0xC2, 0x87, 0xD6, 0x12, 0x00>>,
+      :str_encoded
+    )
+    |> elem(0)
+    |> Util.assert_equals(1_234_567)
+  end
+
+  def consume(<<@next_six, length::6, tl::binary>>, :length_encoded),
+    do: {length, tl} |> IO.inspect(label: "length_encoded: next_six")
+
+  def consume(<<@next_fourteen, length::14, tl::binary>>, :length_encoded),
+    do: {length, tl} |> IO.inspect(label: "length_encoded: next_fourteen")
+
+  def consume(<<@next_thirty_two, _::6, length::32, tl::binary>>, :length_encoded),
+    do: {length, tl} |> IO.inspect(label: "length_encoded: next_thirty_two")
+
+  def consume(<<@special, str_format::6, tl::binary>>, :length_encoded) do
+    case str_format do
+      0 -> {:eight, tl}
+      1 -> {:sixteen, tl}
+      2 -> {:thirtytwo, tl}
+    end
+  end
+
+  def consume(bin, :str_encoded) do
+    case consume(bin, :length_encoded) do
+      {:eight, tl} ->
+        <<int_str, tl::binary>> = tl
+        {int_str, tl} |> IO.inspect(label: "str_encoded:eight")
+
+      {:sixteen, tl} ->
+        <<int_str::16-little, tl::binary>> = tl
+        {int_str, tl} |> IO.inspect(label: "str_encoded:sixteen")
+
+      {:thirtytwo, tl} ->
+        <<int_str::32-little, tl::binary>> = tl
+        {int_str, tl} |> IO.inspect(label: "str_encoded:thirtytwo")
+
+      {length, tl} ->
+        <<str::size(length)-binary, tl::binary>> = tl
+        {str, tl} |> IO.inspect(label: "str_encoded")
+    end
+  end
+
+  def parse(<<@eof, tl::binary>>) do
+    IO.puts("eof")
+    {%Section{kind: :eof, data: nil}, tl}
+  end
+
+  def parse(<<@selectdb, bin::binary>>) do
+    IO.puts("selectdb")
+    {db_index, tl} = consume(bin, :length_encoded) |> IO.inspect()
+    <<@resizedb, tl::binary>> = tl
+    {table_size, tl} = consume(tl, :length_encoded) |> IO.inspect()
+    {expire_table_size, tl} = consume(tl, :length_encoded) |> IO.inspect()
+
+    {%Section{
+       kind: :selectdb,
+       data: %{db_index: db_index, table_size: table_size, expire_table_size: expire_table_size}
+     }, tl}
+  end
+
+  def parse(<<@string, bin::binary>>) do
+    IO.puts("string no expiry")
+    {key, tl} = consume(bin, :str_encoded) |> IO.inspect()
+    {val, tl} = consume(tl, :str_encoded) |> IO.inspect()
+    {%Section{kind: :kv_pair, data: %{key: key, val: val}}, tl}
+  end
+
+  def parse(<<@expiretime, expiretime::32-little, bin::binary>>) do
+    IO.puts("expiretime")
+    {key, tl} = consume(bin, :str_encoded) |> IO.inspect()
+    {val, tl} = consume(tl, :str_encoded) |> IO.inspect()
+    {%Section{kind: :kv_pair, data: %{key: key, val: val, expiretime: expiretime}}, tl}
+  end
+
+  def parse(<<@expiretime_ms, expiretime_ms::64-little, bin::binary>>) do
+    IO.puts("expiretime_ms")
+    {key, tl} = consume(bin, :str_encoded) |> IO.inspect()
+    {val, tl} = consume(tl, :str_encoded) |> IO.inspect()
+    {%Section{kind: :kv_pair, data: %{key: key, val: val, expiretime_ms: expiretime_ms}}, tl}
+  end
+
+  def parse(<<@aux, bin::binary>>) do
+    IO.puts("aux")
+    {key, tl} = consume(bin, :str_encoded) |> IO.inspect()
+    {val, tl} = consume(tl, :str_encoded) |> IO.inspect()
+    {%Section{kind: :aux, data: %{key: key, val: val}}, tl}
+  end
+
+  def parse(<<@header, tl::binary>>) do
+    IO.puts("header")
+    {%Section{kind: :header, data: nil}, tl}
+  end
+
+  def parse_file(bin, acc) do
+    case parse(bin) do
+      {%Section{kind: :eof} = section, _tl} -> Enum.reverse([section | acc])
+      {section, tl} -> parse_file(tl, [section |> IO.inspect() | acc])
+    end
+  end
+end
+
 defmodule Server do
   @array ?*
   @simple_str ?+
@@ -12,6 +178,17 @@ defmodule Server do
   defmodule Ctx do
     @enforce_keys [:config]
     defstruct [:config, socket: nil, client: nil]
+
+    def config_get(%Ctx{config: config}, name) when is_atom(name), do: Map.fetch!(config, name)
+
+    def config_get(%Ctx{config: config}, name),
+      do: Map.fetch!(config, String.to_existing_atom(name))
+
+    def filepath(ctx),
+      do:
+        [config_get(ctx, :dir), config_get(ctx, :dbfilename)]
+        |> Path.join()
+        |> IO.inspect(label: "Filepath")
   end
 
   defmodule Command do
@@ -103,7 +280,24 @@ defmodule Server do
     do:
       :gen_tcp.send(
         ctx.client,
-        [name, Map.fetch!(ctx.config, String.to_existing_atom(name))]
+        [name, Ctx.config_get(ctx, name)]
+        |> Enum.map(&encode(&1, @bulk_str))
+        |> encode(@array)
+      )
+
+  def exec(%Command{kind: "KEYS", args: ["*"]}, ctx),
+    do:
+      :gen_tcp.send(
+        ctx.client,
+        ctx
+        |> Ctx.filepath()
+        |> File.read!()
+        |> Rdb.parse_file([])
+        |> IO.inspect(label: "sections")
+        |> Enum.filter(fn %Rdb.Section{kind: kind} -> kind == :kv_pair end)
+        |> IO.inspect(label: "kv_pairs")
+        |> Enum.map(fn %Rdb.Section{data: %{key: key}} -> key end)
+        |> IO.inspect(label: "keys")
         |> Enum.map(&encode(&1, @bulk_str))
         |> encode(@array)
       )
