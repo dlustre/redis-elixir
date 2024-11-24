6c371f1b3657fd0f471814e992215af0b72246f0
dlustre <dennis.lustre@gmail.com>
2024-11-23 16:14:19 -0800

    codecrafters submit [skip ci]

--- lib/server.ex
+++ lib/server.ex
@@ -69,34 +69,34 @@ defmodule Server do
   end
 
   defmodule Ctx do
-    defstruct socket: nil, client: nil, config: default_config
-
-    def config_get(%Ctx{config: config}, name) when is_atom(name), do: Map.fetch!(config, name)
-
-    def config_get(%Ctx{config: config}, name),
-      do: Map.fetch!(config, String.to_existing_atom(name))
-
-    def filepath(ctx),
-      do:
-        [config_get(ctx, :dir), config_get(ctx, :dbfilename)]
-        |> Path.join()
-        |> IO.inspect(label: "Filepath")
-
-    def file(ctx), do: ctx |> filepath() |> File.read!()
+    defstruct socket: nil, client: nil
   end
+
+  def config_fetch!(key), do: Agent.get(Config, &Map.fetch!(&1, key))
+
+  def filepath(),
+    do:
+      [config_fetch!(:dir), config_fetch!(:dbfilename)]
+      |> Path.join()
+      |> IO.inspect(label: "Filepath")
+
+  def file(), do: filepath() |> File.read!()
+
+  def is_config_key?(key), do: Agent.get(Config, &Map.has_key?(&1, key))
+  def is_replica?(), do: is_config_key?(:replicaof)
 
   defmodule Command do
     @array array
     @bulk_str bulk_str
 
     @enforce_keys [:kind, :args]
-    defstruct [:kind, :args]
+    defstruct [:kind, :args, :resp_bytes]
 
     def encode(%Command{kind: kind, args: args}) do
       [kind | args]
       |> Enum.map(&Resp.encode(&1, @bulk_str))
       |> Resp.encode(@array)
-      |> IO.inspect()
+      |> IO.inspect(label: "encoded command")
     end
 
     def propagate?(%Command{kind: kind}) when kind in ["SET"], do: true
@@ -110,23 +110,24 @@ defmodule Server do
         switches: [port: :integer, dir: :string, dbfilename: :string]
       )
 
-    ctx = %Ctx{config: Enum.into(args, @default_config)} |> IO.inspect()
+    {:ok, _} =
+      Agent.start_link(fn -> Enum.into(args, @default_config) end, name: Config)
+      |> IO.inspect(label: "started config agent")
 
-    Supervisor.start_link([{Task, fn -> Server.listen(ctx) end}], strategy: :one_for_one)
+    Supervisor.start_link([{Task, fn -> Server.listen(%Ctx{}) end}], strategy: :one_for_one)
   end
 
-  # def consume_commands(<<>>, acc), do: Enum.reverse(acc)
-
-  # def consume_commands(bin, acc) do
-  #   {command, tl} = Server.command(bin)
-  #   consume_commands(tl, [command | acc])
-  # end
-
-  def handle_commands(<<>>, _), do: IO.inspect("finished queueing commands")
+  def handle_commands(<<>>, _), do: IO.inspect("finished commands")
 
   def handle_commands(bin, func) do
     {command, tl} = command(bin)
+    IO.inspect("Here")
     func.(command)
+
+    Agent.update(Config, fn config ->
+      Map.update!(config, :master_repl_offset, &Kernel.+(&1, command.resp_bytes))
+    end)
+
     handle_commands(tl, func)
   end
 
@@ -134,7 +135,7 @@ defmodule Server do
 
   def command(
         <<@array, args_length_str::8-bitstring, @sep, @bulk_str, kind_length_str::8-bitstring,
-          @sep, tl::binary>> = _bin
+          @sep, tl::binary>> = bin
       ) do
     kind_length = String.to_integer(kind_length_str)
     <<kind::size(kind_length)-unit(8)-binary, @sep, tl::binary>> = tl
@@ -147,7 +148,9 @@ defmodule Server do
         {[arg | args], tl}
       end)
 
-    {%Command{kind: kind, args: Enum.reverse(args)}, tl} |> IO.inspect(label: "Parsed command")
+    {%Command{kind: kind, args: Enum.reverse(args), resp_bytes: byte_size(bin) - byte_size(tl)},
+     tl}
+    |> IO.inspect(label: "Parsed command")
   end
 
   def set(kv_pair) do
@@ -155,86 +158,85 @@ defmodule Server do
     :ets.tab2list(:redis) |> IO.inspect(label: "result of set")
   end
 
-  def exec(%Command{kind: "PING"}, ctx), do: :gen_tcp.send(ctx.client, @pong)
+  def exec(command, ctx) do
+    IO.inspect({command, ctx}, label: "executing command")
+    do_exec(command, ctx)
+  end
 
-  def exec(%Command{kind: "ECHO", args: [msg]}, ctx),
+  def do_exec(%Command{kind: "PING"}, ctx) do
+    IO.inspect(is_replica?(), label: "ping")
+    if not is_replica?(), do: :gen_tcp.send(ctx.client, @pong)
+  end
+
+  def do_exec(%Command{kind: "ECHO", args: [msg]}, ctx),
     do: :gen_tcp.send(ctx.client, Resp.encode(msg, @bulk_str))
 
-  def exec(%Command{kind: "SET", args: [key, value, "px", expiry_ms]}, ctx) do
+  def do_exec(%Command{kind: "SET", args: [key, value, "px", expiry_ms]}, ctx) do
     expiration = :os.system_time(:millisecond) + String.to_integer(expiry_ms)
     set({key, value, expiration})
-    if !is_map_key(ctx.config, :replicaof), do: :gen_tcp.send(ctx.client, ok())
+    if not is_replica?(), do: :gen_tcp.send(ctx.client, ok())
   end
 
-  def exec(%Command{kind: "SET", args: [key, value]}, ctx) do
+  def do_exec(%Command{kind: "SET", args: [key, value]}, ctx) do
     set({key, value})
-    if !is_map_key(ctx.config, :replicaof), do: :gen_tcp.send(ctx.client, ok())
+    if not is_replica?(), do: :gen_tcp.send(ctx.client, ok())
   end
 
-  def exec(
-        %Command{kind: "GET", args: [key]},
-        %Ctx{
-          config: %{dir: _, dbfilename: _},
-          client: client
-        } = ctx
-      ),
-      do:
-        :gen_tcp.send(
-          client,
-          Rdb.parse_file(Ctx.file(ctx), [])
-          |> elem(0)
-          |> Enum.find_value(fn
-            %Rdb.Section{
-              kind: :kv_pair,
-              data: %{key: candidate, val: val, expiretime: {expiretime, time_unit}}
-            }
-            when candidate == key ->
-              if expiretime <= :os.system_time(time_unit),
-                do: @null_bulk_str,
-                else: Resp.encode(val, @bulk_str)
-
-            %Rdb.Section{kind: :kv_pair, data: %{key: candidate, val: val}}
-            when candidate == key ->
-              Resp.encode(val, @bulk_str)
-
-            _ ->
-              nil
-          end)
-          |> IO.inspect(label: "value for: " <> key)
-        )
-
-  def exec(%Command{kind: "GET", args: [key]}, ctx) do
+  def do_exec(%Command{kind: "GET", args: [key]}, ctx) do
     value =
-      case :ets.lookup(:redis, key) |> IO.inspect(label: key <> " when looked up:") do
-        [] ->
-          raise "'#{key}' not found in ets"
+      if is_config_key?(:dir) and is_config_key?(:dbfilename) do
+        Rdb.parse_file(file(), [])
+        |> elem(0)
+        |> Enum.find_value(fn
+          %Rdb.Section{
+            kind: :kv_pair,
+            data: %{key: candidate, val: val, expiretime: {expiretime, time_unit}}
+          }
+          when candidate == key ->
+            if expiretime <= :os.system_time(time_unit),
+              do: @null_bulk_str,
+              else: Resp.encode(val, @bulk_str)
 
-        [{^key, value, expiration}] ->
-          if expiration <= :os.system_time(:millisecond),
-            do: @null_bulk_str,
-            else: Resp.encode(value, @bulk_str)
+          %Rdb.Section{kind: :kv_pair, data: %{key: candidate, val: val}}
+          when candidate == key ->
+            Resp.encode(val, @bulk_str)
 
-        [{^key, value}] ->
-          Resp.encode(value, @bulk_str)
+          _ ->
+            nil
+        end)
+        |> IO.inspect(label: "value for: " <> key)
+      else
+        case :ets.lookup(:redis, key) do
+          [] ->
+            raise "'#{key}' not found in ets"
+
+          [{^key, value, expiration}] ->
+            if expiration <= :os.system_time(:millisecond),
+              do: @null_bulk_str,
+              else: Resp.encode(value, @bulk_str)
+
+          [{^key, value}] ->
+            Resp.encode(value, @bulk_str)
+        end
       end
 
     :gen_tcp.send(ctx.client, value)
   end
 
-  def exec(%Command{kind: "CONFIG", args: ["GET", name]}, ctx),
+  def do_exec(%Command{kind: "CONFIG", args: ["GET", name]}, ctx),
     do:
       :gen_tcp.send(
         ctx.client,
-        [name, Ctx.config_get(ctx, name)]
+        [name, config_fetch!(String.to_existing_atom(name))]
         |> Enum.map(&Resp.encode(&1, @bulk_str))
         |> Resp.encode(@array)
       )
 
-  def exec(%Command{kind: "KEYS", args: ["*"]}, ctx),
+  def do_exec(%Command{kind: "KEYS", args: ["*"]}, ctx),
     do:
       :gen_tcp.send(
         ctx.client,
-        Rdb.parse_file(Ctx.file(ctx), [])
+        Rdb.parse_file(file(), [])
         |> elem(0)
         |> IO.inspect(label: "sections")
         |> Enum.filter(fn
@@ -248,24 +250,10 @@ defmodule Server do
         |> Resp.encode(@array)
       )
 
-  def exec(%Command{kind: "INFO", args: _args}, %Ctx{
-        config: %{replicaof: _} = config,
-        client: client
-      }) do
+  def do_exec(%Command{kind: "INFO", args: _args}, ctx) do
     replication =
-      Map.take(config, [:master_replid, :master_repl_offset]) |> Map.merge(%{role: "slave"})
-
-    res =
-      Enum.map_join(replication, "\n", fn {key, val} -> "#{key}:#{val}" end)
-      |> Resp.encode(@bulk_str)
-      |> IO.inspect()
-
-    :gen_tcp.send(client, res)
-  end
-
-  def exec(%Command{kind: "INFO", args: _args}, ctx) do
-    replication =
-      Map.take(ctx.config, [:master_replid, :master_repl_offset]) |> Map.merge(%{role: "master"})
+      Agent.get(Config, &Map.take(&1, [:master_replid, :master_repl_offset]))
+      |> Map.merge(%{role: if(is_replica?(), do: "slave", else: "master")})
 
     res =
       Enum.map_join(replication, "\n", fn {key, val} -> "#{key}:#{val}" end)
@@ -275,81 +263,66 @@ defmodule Server do
     :gen_tcp.send(ctx.client, res)
   end
 
-  def exec(%Command{kind: @replconf, args: ["capa" | _] = capabilities}, ctx) do
+  def do_exec(%Command{kind: @replconf, args: ["capa" | _] = capabilities}, ctx) do
     IO.puts("capa: #{capabilities}")
     :gen_tcp.send(ctx.client, ok())
   end
 
-  def exec(%Command{kind: @replconf, args: ["listening-port", _]}, ctx),
+  def do_exec(%Command{kind: @replconf, args: ["listening-port", _]}, ctx),
     do: :gen_tcp.send(ctx.client, ok())
 
-  def exec(%Command{kind: @replconf, args: ["GETACK", "*"]}, ctx) do
+  def do_exec(%Command{kind: @replconf, args: ["GETACK", "*"]}, %Ctx{client: client}) do
     IO.puts("getack: *")
 
     :gen_tcp.send(
-      ctx.client,
-      [@replconf, "ACK", "0"]
+      client,
+      [@replconf, "ACK", config_fetch!(:master_repl_offset) |> Integer.to_string()]
       |> Enum.map(&Resp.encode(&1, @bulk_str))
       |> Resp.encode(@array)
     )
   end
 
-  def exec(
-        %Command{kind: @psync, args: [replid, repl_offset]},
-        %Ctx{config: %{master_replid: master_replid}, client: client}
-      ) do
+  def do_exec(%Command{kind: @psync, args: [replid, repl_offset]}, ctx) do
     IO.puts("psync: #{replid}, #{repl_offset}")
 
-    :ok = :gen_tcp.send(client, Resp.encode("FULLRESYNC #{master_replid} 0", @simple_str))
-    :ok = :gen_tcp.send(client, Rdb.empty_file() |> Resp.encode_file())
+    :ok =
+      :gen_tcp.send(
+        ctx.client,
+        Resp.encode("FULLRESYNC #{config_fetch!(:master_replid)} 0", @simple_str)
+      )
 
-    Agent.update(ReplicaSet, fn rs -> MapSet.put(rs, client) end)
+    :ok = :gen_tcp.send(ctx.client, Rdb.empty_file() |> Resp.encode_file())
+
+    Agent.update(ReplicaSet, &MapSet.put(&1, ctx.client))
 
     Agent.get(ReplicaSet, & &1)
     |> IO.inspect(label: "psync successful, adding client to replicaset")
   end
 
-  def exec(unknown_cmd, ctx) do
+  def do_exec(unknown_cmd, ctx) do
     IO.inspect(unknown_cmd)
     IO.inspect(ctx)
     raise "Unexpected command: " <> unknown_cmd.kind
   end
 
-  def serve(%Ctx{config: %{replicaof: _}} = ctx) do
+  def serve(ctx) do
     case :gen_tcp.recv(ctx.client, 0) do
       {:ok, data} ->
-        IO.inspect("recvd as replica")
-        handle_commands(data, &exec(&1, ctx))
+        handle_commands(data, fn command ->
+          exec(command, ctx)
+          # TODO: make concurrent
+          if not is_replica?() and Command.propagate?(command),
+            do:
+              Agent.get(ReplicaSet, & &1)
+              |> IO.inspect(label: "propagating to these replicas")
+              |> Enum.map(&:gen_tcp.send(&1, Command.encode(command)))
+              |> IO.inspect(label: "propagation result")
+        end)
+
         serve(ctx)
 
       {:error, :closed} ->
-        IO.puts("Socket closed")
-
-      msg ->
-        IO.inspect(msg)
-        raise "Unknown"
-    end
-  end
-
-  def serve(%Ctx{config: config} = ctx) when not is_map_key(config, :replicaof) do
-    case :gen_tcp.recv(ctx.client, 0) do
-      {:ok, data} ->
-        IO.inspect("recvd as master")
-        {command, _tl} = command(data)
-
-        # TODO: make concurrent
-        if Command.propagate?(command),
-          do:
-            Agent.get(ReplicaSet, & &1)
-            |> IO.inspect(label: "propagating to these replicas")
-            |> Enum.map(&:gen_tcp.send(&1, Command.encode(command)))
-            |> IO.inspect(label: "propagation result")
-
-        exec(command, ctx)
-        serve(ctx)
-
-      {:error, :closed} ->
-        IO.puts("Socket closed")
+        IO.inspect("Socket closed")
 
       msg ->
         IO.inspect(msg)
@@ -368,7 +341,9 @@ defmodule Server do
     loop_acceptor(ctx)
   end
 
-  def send_handshake(%Ctx{config: %{port: port, replicaof: replicaof}} = ctx) do
+  def send_handshake(ctx) do
+    port = config_fetch!(:port)
+    replicaof = config_fetch!(:replicaof)
     [master_base, master_port] = String.split(replicaof)
 
     {:ok, master_socket} =
@@ -421,30 +396,34 @@ defmodule Server do
     {file, propagated_commands} = Resp.decode_file(encoded_file)
     {_, <<>>} = Rdb.parse_file(file, [])
 
-    handle_commands(propagated_commands, &exec(&1, %Ctx{ctx | client: master_socket}))
+    ctx = %Ctx{ctx | client: master_socket}
 
-    Task.start_link(fn -> serve(%Ctx{ctx | client: master_socket}) end)
+    handle_commands(propagated_commands, &exec(&1, ctx))
+    Task.start_link(fn -> serve(ctx) end)
   end
 
   @doc """
   Listen for incoming connections
   """
-  def listen(%Ctx{config: %{replicaof: _}} = ctx) do
-    send_handshake(ctx)
+  def listen(ctx) do
+    if is_replica?(), do: send_handshake(ctx)
     do_listen(ctx)
   end
 
-  def listen(ctx), do: do_listen(ctx)
-
   def do_listen(ctx) do
+    port = config_fetch!(:port)
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} =
-      :gen_tcp.listen(ctx.config.port, [:binary, active: false, reuseaddr: true])
-      |> IO.inspect(label: "Listening to port: #{ctx.config.port}")
+      :gen_tcp.listen(port, [:binary, active: false, reuseaddr: true])
+      |> IO.inspect(label: "Listening to port: #{port}")
 
     :ets.new(:redis, [:set, :public, :named_table])
-    {:ok, _} = Agent.start_link(&MapSet.new/0, name: ReplicaSet)
+
+    {:ok, _} =
+      Agent.start_link(&MapSet.new/0, name: ReplicaSet)
+      |> IO.inspect(label: "started replicaset agent")
+
     loop_acceptor(%Ctx{ctx | socket: socket})
   end
 end
